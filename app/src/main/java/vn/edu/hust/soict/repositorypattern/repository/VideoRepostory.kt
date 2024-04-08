1024037f81fef708c90c17b60e8b9ff62b1b6ed7
Minh Dang <dang14072k2@gmail.com>
2024-04-08 16:28:47 +0700

    feat: add project repository pattern

---
+++ app/src/main/java/vn/edu/hust/soict/repositorypattern/repository/VideoRepostory.kt
@@ -0,0 +1,6 @@
+package vn.edu.hust.soict.repositorypattern.repository
+
+/**
+ * Repository for fetching devbyte videos from the network and storing them on disk
+ */
+// TODO: Implement the VideosRepository class
